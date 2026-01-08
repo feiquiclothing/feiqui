7c30e8fe40765417a081eefbee33c0dfe70c3b2f
FEIQUI <adrianantunez@outlook.com>
2026-01-08 13:00:27 -0300

    Create Maintenance.tsx

---
+++ app/Maintenance.tsx
@@ -0,0 +1,19 @@
+export default function Maintenance() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "white",
+        color: "#6f6f6f",
+        fontSize: 12,
+        letterSpacing: 1,
+        textTransform: "uppercase",
+      }}
+    >
+      SITIO EN REMODELACIÓN
+    </main>
+  );
+}
